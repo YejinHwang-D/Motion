8378eeac66a1768069564a7e8754832c88c1d9e1
YejinHwang-D <hyj3463@naver.com>
2023-01-28 17:59:21 +0900

    feat: 이미지 컴포넌트💡

--- build/app.js
+++ build/app.js
@@ -1,8 +1,11 @@
 import { PageComponent } from './component/Page.js';
+import { ImageComponent } from './component/Menu/Image.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import { PageComponent } from './component/Page.js';
+import { ImageComponent } from './component/Menu/Image.js';
 
 class App {
     private readonly page: PageComponent;
+
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 
